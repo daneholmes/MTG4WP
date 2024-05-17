dc45b11850801f684bece02039cf5861141749e0
Dane Holmes <dane@danes-macbook-air.local>
2024-05-17 15:37:12 -0500

    Added a tooltip shortcode

---
+++ src/tooltips.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+	tippy('.mtg-tooltip', {
+		allowHTML: true,
+		interactive: true,
+		placement: 'bottom',
+		followCursor: 'horizontal',
+	});
+});
